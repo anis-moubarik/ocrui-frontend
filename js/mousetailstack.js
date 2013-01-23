cba74e16ed6ce509993e038f9ae3e00b670e4ab5
Juho Vuori <juho.vuori@helsinki.fi>
2013-01-23 18:48:02 +0200

    file forgotten

---
+++ js/mousetailstack.js
@@ -0,0 +1,90 @@
+/*globals console:true setTimeout:false */
+define(['jquery','toolbar','events','backbone','mousetailstack'],function ($,toolbar,events,Backbone,mousetailstack) {
+    "use strict";
+
+    function init(ev) {
+        this.stack = [ev];
+        this.dx = 0;
+        this.dy = 0;
+    }
+
+    function push (ev) {
+        this.stack.push(ev);
+
+        this.removeStaleElements(ev);
+    }
+
+    function removeStaleElements (ev) {
+
+        // BUG: this assumes events are pushed in timestamp order
+        var i = 0;
+        while ((i < this.stack.length) &&
+                (ev.timeStamp - this.stack[i].timeStamp > this.timeTolerance)) {
+            i += 1;
+        }
+
+        this.stack.splice(0,i);
+
+    }
+
+    function truncate(n) {
+        return Math[n > 0 ? "floor" : "ceil"](n);
+    }
+
+    function triggerTail() {
+
+        var that = this;
+
+        this.dx = truncate(this.dx*this.length);
+        this.dy = truncate(this.dy*this.length);
+
+        if ((this.dx != 0) && (this.dy != 0)) {
+            setTimeout( function() {
+                that.triggerTail();
+            }, 50);
+            events.trigger('mousetail',[this.dx,this.dy]);
+        }
+
+
+    }
+
+    function end (ev) {
+        
+        this.removeStaleElements(ev);
+
+        // Sometimes (mouseout) there is no proper coordinates
+        // on events. Just give up if that happens
+        try {
+            var ev0 = this.stack[0];
+            this.dx = ev.pageX - ev0.pageX;
+            this.dy = ev.pageY - ev0.pageY;
+        } catch (err) {
+            return;
+        }
+
+        this.triggerTail();
+
+    }
+
+    // This is used to implement nice non-sticky panning
+    function MouseTailStack(callback,timeTolerance) {
+
+        this.stack = [];
+        this.length = 0.6; // 0 <= length < 1
+        this.callback = callback;
+        this.init = init;
+        this.push = push;
+        this.removeStaleElements = removeStaleElements;
+        this.end = end;
+        this.triggerTail = triggerTail;
+        if (timeTolerance === undefined) {
+            timeTolerance = 100; // milliseconds
+        }
+        this.timeTolerance = timeTolerance
+    }
+
+    return {
+        MouseTailStack: MouseTailStack
+    }
+
+});
