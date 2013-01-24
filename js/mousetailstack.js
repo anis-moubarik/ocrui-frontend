5b2717f215ba065a15bce95299157157c6990f5e
Juho Vuori <juho.vuori@helsinki.fi>
2013-01-24 09:17:15 +0200

    Tiny code cleanup in mousetail

--- js/mousetailstack.js
+++ js/mousetailstack.js
@@ -2,25 +2,54 @@
 define(['jquery','toolbar','events','backbone','mousetailstack'],function ($,toolbar,events,Backbone,mousetailstack) {
     "use strict";
 
+    // This is used to implement nice non-sticky panning
+    function MouseTailStack(callback,timeTolerance) {
+
+        if (timeTolerance === undefined) {
+
+            timeTolerance = 100; // milliseconds
+
+        }
+
+        this.stack = [];
+        this.length = 0.6; // 0 <= length < 1
+        this.callback = callback;
+        this.init = init;
+        this.push = push;
+        this.removeStaleElements = removeStaleElements;
+        this.end = end;
+        this.triggerTail = triggerTail;
+        this.timeTolerance = timeTolerance
+
+    }
+
     function init(ev) {
+
         this.stack = [ev];
         this.dx = 0;
         this.dy = 0;
+
     }
 
     function push (ev) {
-        this.stack.push(ev);
 
+        this.stack.push(ev);
         this.removeStaleElements(ev);
+
     }
 
     function removeStaleElements (ev) {
 
-        // BUG: this assumes events are pushed in timestamp order
+        // This assumes events are pushed in timestamp order
+        // This might not be strictly valid assumption but don't worry
+        // about it, this is good enough
         var i = 0;
+
         while ((i < this.stack.length) &&
                 (ev.timeStamp - this.stack[i].timeStamp > this.timeTolerance)) {
+
             i += 1;
+
         }
 
         this.stack.splice(0,i);
@@ -28,7 +57,9 @@ define(['jquery','toolbar','events','backbone','mousetailstack'],function ($,too
     }
 
     function truncate(n) {
+
         return Math[n > 0 ? "floor" : "ceil"](n);
+
     }
 
     function triggerTail() {
@@ -39,10 +70,10 @@ define(['jquery','toolbar','events','backbone','mousetailstack'],function ($,too
         this.dy = truncate(this.dy*this.length);
 
         if ((this.dx != 0) && (this.dy != 0)) {
-            setTimeout( function() {
-                that.triggerTail();
-            }, 50);
+
+            setTimeout( function() { that.triggerTail(); }, 50);
             events.trigger('mousetail',[this.dx,this.dy]);
+
         }
 
 
@@ -54,33 +85,21 @@ define(['jquery','toolbar','events','backbone','mousetailstack'],function ($,too
 
         // Sometimes (mouseout) there is no proper coordinates
         // on events. Just give up if that happens
+
         try {
+
             var ev0 = this.stack[0];
             this.dx = ev.pageX - ev0.pageX;
             this.dy = ev.pageY - ev0.pageY;
+
         } catch (err) {
+
             return;
+
         }
 
         this.triggerTail();
 
-    }
-
-    // This is used to implement nice non-sticky panning
-    function MouseTailStack(callback,timeTolerance) {
-
-        this.stack = [];
-        this.length = 0.6; // 0 <= length < 1
-        this.callback = callback;
-        this.init = init;
-        this.push = push;
-        this.removeStaleElements = removeStaleElements;
-        this.end = end;
-        this.triggerTail = triggerTail;
-        if (timeTolerance === undefined) {
-            timeTolerance = 100; // milliseconds
-        }
-        this.timeTolerance = timeTolerance
     }
 
     return {
