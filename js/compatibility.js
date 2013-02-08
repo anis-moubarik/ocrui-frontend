2ad264d39329e14af02e6f8aade4a384fbdd5535
Juho Vuori <juho.vuori@helsinki.fi>
2013-02-08 16:25:13 +0200

    som eie8 support

--- js/compatibility.js
+++ js/compatibility.js
@@ -1,5 +1,13 @@
 define(['jquery','underscore','jsdiff','utils'],function ($,_,jsdiff,utils) {
     "use strict";
 
+    // IE8...
+
+    if (!Date.now) {
+        Date.now = function() {
+            return new Date().valueOf();
+        }
+    }
+
     return;
 });
