728113b067012f7b7829348969528ef7b378d4ab
Anis Moubarik <anis.moubarik@helsinki.fi>
2015-04-28 16:16:04 +0300

    test tag with multiple words

--- js/alto.js
+++ js/alto.js
@@ -59,6 +59,7 @@ define(['underscore','jquery','libwords','mybackbone','ocruidoc','events','wordc
         setNthWordLanguage: function(index,language) {
             this.editorWords[index].language = language;
             this.changedSinceSave = true;
+            this.changedSince0 = true;
             return this.editorWords[index];
         },
         setNthWordTag: function(index, tag) {
